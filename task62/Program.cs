ce3a38ceb83cfe68fc2966b0c73c5494d3630123
Наталья <109668015+andre19071@users.noreply.github.com>
2022-09-02 14:15:17 +0700

    task62

--- task62/Program.cs
+++ task62/Program.cs
@@ -4,3 +4,53 @@
 // 12 13 14 05
 // 11 16 15 06
 // 10 09 08 07
+
+Console.Write("Введите количество строк: ");
+int lines = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+int[,] numbers = new int[lines, columns];
+Console.Write("Введите начальный элемент матрицы: ");
+int val = Convert.ToInt32(Console.ReadLine());
+
+int i = 0, k = 0, j;
+
+while (i < lines * columns)
+{
+    k++;
+    for (j = k - 1; j < columns - k + 1; j++)
+    {
+        numbers[k - 1, j] = val++;
+        i++;
+    }
+
+    for (j = k; j < lines - k + 1; j++)
+    {
+        numbers[j, columns - k] = val++;
+        i++;
+    }
+
+    for (j = columns - k - 1; j >= k - 1; j--)
+    {
+        numbers[lines - k, j] = val++;
+        i++;
+    }
+
+    for (j = lines - k - 1; j >= k; j--)
+    {
+        numbers[j, k - 1] = val++;
+        i++;
+    }
+
+}
+PrintArray(numbers);
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i, j] + "; ");
+        Console.WriteLine();
+    }
+}
+
